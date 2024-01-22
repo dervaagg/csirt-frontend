37113110a87a2ea6cd30a6940f9f0073c7fc1bb6
Thuffail Arsyad <thuffailars20@gmail.com>
2024-01-22 08:27:24 +0700

    22/01/24

--- src/components/CyberHeat.jsx
+++ src/components/CyberHeat.jsx
@@ -3,13 +3,7 @@ export default function CyberHeat() {
       <div id="Cyberheat">
         <div className="container cyberheat-container">
           <iframe
-<<<<<<< HEAD
             src="https://threatmap.fortiguard.com/"          
-=======
-            width="900"
-            height="550"
-            src="https://threatmap.fortiguard.com/"
->>>>>>> 1bf635882d3e2f5eb89dc394d60ce5c82f950b05
             border="0"
             sandbox="allow-scripts allow-same-origin"
             allowFullScreen
