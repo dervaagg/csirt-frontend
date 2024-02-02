ca0f886af1ea2cf7b87f43f024447952ec9bf64a
Thuffail Arsyad <thuffailars20@gmail.com>
2024-02-02 15:32:29 +0700

    update tampilan

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import PageNotFound from './pages/PagenotFound.jsx'
 import Admin from './pages/Admin.jsx'
 import App from './App.jsx'
 import './index.css'
@@ -10,9 +9,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/*" element={<App />} />
         <Route path="/31937129843/admin" element={<Admin />} />
-        <Route path="/*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>  
   </React.StrictMode>,
