581992131e5cca7aa2ca5921877cb200836ce819
Thuffail Arsyad <thuffailars20@gmail.com>
2024-02-02 14:13:34 +0700

    update path

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<App />} />
+        <Route path="/" element={<App />} />
         <Route path="/31937129843/admin" element={<Admin />} />
         <Route path="/*" element={<PageNotFound />} />
       </Routes>
