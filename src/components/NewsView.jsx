272329bbd5ba6c8c578372be929e9beafbf43258
dervaag <dervaag1122@gmail.com>
2024-02-14 16:44:54 +0700

    updated minor bug

--- src/components/NewsView.jsx
+++ src/components/NewsView.jsx
@@ -1,4 +1,3 @@
-
 import { useParams } from "react-router-dom";
 import "../css/NewsViewer.css";
 import axios from "axios";
@@ -28,35 +27,35 @@ export default function NewsView() {
   const { id } = useParams();
   const news = newses.find((news) => news.id === Number(id));
 
-return (
-  <div className="container-newsview">
-    {news && (
-      <>
-      <div className="container header-news">
-        <img src={news.url} alt="" />
-        <div className="title">
-          <h3>{news.title}</h3>
-          <small>{news.category}</small>
-        </div>
-      </div>
-      <div className="container content-news">
-        <div className="container-sumber">
-          <p>
-            <a href={news.source}></a>
-          </p>
-          <p>Tanggal {news.date}</p>
-        </div>
-        <div className="text">
-          <p className="sub-title">{news.sub_title1}</p>
-          <p>{news.paragraph1}</p>
-          <p className="sub-title">{news.sub_title2}</p>
-          <p>{news.paragraph2}</p>
-          <p className="sub-title">{news.sub_title3}</p>
-          <p>{news.paragraph3}</p>
-        </div>
-      </div>
-      </>      
-    )}
-  </div>
-);
+  return (
+    <div className="container-newsview">
+      {news && (
+        <>
+          <div className="container header-news">
+            <img src={news.url} alt="" />
+            <div className="title">
+              <h3>{news.title}</h3>
+              <small>{news.category}</small>
+            </div>
+          </div>
+          <div className="container content-news">
+            <div className="container-sumber">
+              <p>
+                <a href={news.source}></a>
+              </p>
+              <p>{news.date}</p>
+            </div>
+            <div className="text">
+              <p className="sub-title">{news.sub_title1}</p>
+              <p>{news.paragraph1}</p>
+              <p className="sub-title">{news.sub_title2}</p>
+              <p>{news.paragraph2}</p>
+              <p className="sub-title">{news.sub_title3}</p>
+              <p>{news.paragraph3}</p>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
